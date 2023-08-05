a8152ddca8e967fce32cf6ac462e33127c845568
Dogui <douggui.med.amine@gmail.com>
2023-08-05 20:11:10 +0200

    add sns

---
+++ sns.tf
@@ -0,0 +1,28 @@
+resource "aws_sns_topic" "sns_topic" {
+  name            = "Sensor-Topic"
+  delivery_policy = <<EOF
+{
+  "http": {
+    "defaultHealthyRetryPolicy": {
+      "minDelayTarget": 20,
+      "maxDelayTarget": 20,
+      "numRetries": 3,
+      "numMaxDelayRetries": 0,
+      "numNoDelayRetries": 0,
+      "numMinDelayRetries": 0,
+      "backoffFunction": "linear"
+    },
+    "disableSubscriptionOverrides": false,
+    "defaultThrottlePolicy": {
+      "maxReceivesPerSecond": 1
+    }
+  }
+}
+EOF
+}
+
+resource "aws_sns_topic_subscription" "sns_sub" {
+  topic_arn = aws_sns_topic.sns_topic.arn
+  protocol  = "email"
+  endpoint  = "douggui.med.amine@gmail.com"
+}
